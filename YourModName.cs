ea1ca312b7ed7ba03bd23a93c53ae19bd7a58587
Pixelstorm <thepixelstormer@gmail.com>
2023-08-09 13:19:54 +0100

    Init from template

---
+++ YourModName.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+
+namespace yourmodname
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class YourModName : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = MyPluginInfo.PLUGIN_GUID;
+        public const string PLUGIN_NAME = MyPluginInfo.PLUGIN_NAME;
+        public const string PLUGIN_VERSION = MyPluginInfo.PLUGIN_VERSION;
+
+        private void Awake()
+        {
+            Logger.LogInfo($"Hello Rain World from {PLUGIN_NAME}!");
+        }
+    }
+}

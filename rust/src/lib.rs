364572c7f50af994182a2e0c91266f7376457231
Pixelstorm <thepixelstormer@gmail.com>
2023-08-22 22:08:45 +0100

    Improve utf16 string handling

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,13 +1,12 @@
 use std::{net::ToSocketAddrs, sync::Mutex};
 
-use anyhow::bail;
 use bevy::{
     app::{AppExit, ScheduleRunnerPlugin},
     ecs::event::ManualEventReader,
     prelude::*,
     tasks::{AsyncComputeTaskPool, ComputeTaskPool, IoTaskPool},
 };
-use widestring::{u16cstr, U16CStr, U16CString};
+use widestring::{U16CStr, U16CString, Utf16Str};
 
 struct AppContainer {
     pub app: App,
@@ -33,23 +32,24 @@ pub struct MovementDelta {
     y: f32,
 }
 
-extern "C" fn default_error_handler(error: *const u16) {}
+extern "C" fn default_error_handler(error: *const u16) {
+    let string = unsafe { U16CStr::from_ptr_str(error) };
+    let display = string.display();
+    panic!("{display}");
+}
 
 #[no_mangle]
 pub extern "C" fn set_error_handler(handler: ErrorHandlerFn) {
     *ERROR_HANDLER.lock().unwrap() = handler;
 }
 
-fn handle_error(error: impl AsRef<str>) {
+fn handle_error(error: &str) {
+    let error = error.replace('\0', "�");
+    // SAFETY: We just replaced all null bytes in the string, so this is always safe
+    let utf16 = unsafe { U16CString::from_str_unchecked(error) };
     let handler = ERROR_HANDLER.lock().unwrap();
-    match U16CString::from_str(error.as_ref()) {
-        Ok(utf16) => handler(utf16.into_raw()),
-        Err(e) => {
-            let string = error.as_ref();
-            println!("[Rust] Could not utf16-encode error message '{string}': {e}");
-            handler(u16cstr!("Encountered an error while attempting to display a previous error. See console log for details.").as_ptr());
-        }
-    }
+    // SAFETY: The CLR copies strings when marshalling, so the C# code never actually sees this pointer
+    handler(utf16.as_ptr());
 }
 
 #[no_mangle]
@@ -119,26 +119,23 @@ pub extern "C" fn query_movement_delta() -> MovementDelta {
         .unwrap_or_default()
 }
 
+/// # Safety
+///
+/// The caller must ensure the given pointer points to a null-terminated, UTF-16 encoded string
 #[no_mangle]
 pub unsafe extern "C" fn connect_to_server(address: *const u16, port: u16) {
-    if let Err(e) = try_connect_to_server(address, port) {
-        let address = U16CStr::from_ptr_str(address);
-        let display = address.display();
-        handle_error(format!(
-            "Cannot connect to address '{display}' on port {port}:\n\n{e}"
+    let address = U16CStr::from_ptr_str(address);
+    let address = Utf16Str::from_ucstr_unchecked(address);
+    let address = address.to_string();
+
+    if let Err(e) = try_connect_to_server(&address, port) {
+        handle_error(&format!(
+            "Cannot connect to address '{address}' on port {port}:\n\n{e}"
         ));
     }
 }
 
-unsafe fn try_connect_to_server(address: *const u16, port: u16) -> anyhow::Result<()> {
-    let address = U16CStr::from_ptr_str(address);
-
-    if address.is_empty() {
-        bail!("Address is empty.");
-    }
-
-    let address = address.to_string()?;
-
+fn try_connect_to_server(address: &str, port: u16) -> anyhow::Result<()> {
     println!("[Rust] Connecting to address: {address} on port: {port}");
 
     let addresses = (address, port).to_socket_addrs()?;
